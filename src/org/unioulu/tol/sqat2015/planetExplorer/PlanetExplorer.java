3177ce45ccfb8199632b979ef23e6b185363cfca
somename <someemail>
2015-10-20 14:58:49 +0300

    besouro automatic message

---
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -0,0 +1,27 @@
+package org.unioulu.tol.sqat2015.planetExplorer;
+
+public class PlanetExplorer {
+	public PlanetExplorer(int x, int y, String obstacles){
+	/*	x and y represent the size of the grid.
+	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
+	 *  
+		Example use:
+		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  //A 100x100 grid with two obstacles at coordinates (5,5) and (7,8) 
+	 */
+	}
+	
+	public String executeCommand(String command){
+		
+		/* The command string is composed of "f" (forward), "b" (backward), "l" (left) and "r" (right)
+		 * Example: 
+		 * The explorer is on a 100x100 grid at location (0, 0) and facing NORTH. 
+		 * The explorer is given the commands "ffrff" and should end up at (2, 2) facing East.
+		 
+		 * The return string is in the format: "(x,y,facing)(obs1_x,obs1_y)(obs2_x,obs2_y)..(obsN_x,obsN_y)" 
+		 * Where x and y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
+		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
+		 */
+		
+		return null;
+	}
+}
---
+++ src/org/unioulu/tol/sqat2015/planetExplorer/tests/TestPlanetExplorer.java
@@ -0,0 +1,13 @@
+package org.unioulu.tol.sqat2015.planetExplorer.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPlanetExplorer {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}

0f699699f0c3cf93af4a8b6b6f44686f43dd1c2d
Itir Karac <karacitir@gmail.com>
2015-10-20 17:11:53 +0300

    Comments for submission added

--- src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/PlanetExplorer.java
@@ -1,6 +1,6 @@
 package org.unioulu.tol.sqat2015.planetExplorer;
 
-
+// Before submitting write your ID and finish time here. Your ID is written on project description sheets.
 // ID:
 // Finish time:
 public class PlanetExplorer {
@@ -20,8 +20,8 @@ public class PlanetExplorer {
 		 * The explorer is on a 100x100 grid at location (0, 0) and facing NORTH. 
 		 * The explorer is given the commands "ffrff" and should end up at (2, 2) facing East.
 		 
-		 * The return string is in the format: "(x,y,facing)(obs1_x,obs1_y)(obs2_x,obs2_y)..(obsN_x,obsN_y)" 
-		 * Where x and y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
+		 * The return string is in the format: "(pos_x,pos_y,facing)(obs1_x,obs1_y)(obs2_x,obs2_y)..(obsN_x,obsN_y)" 
+		 * Where pos_x and pos_y are the final coordinates, facing is the current direction the explorer is pointing to (N,S,W,E).
 		 * The return string should also contain a list of coordinates of the encountered obstacles. No white spaces.
 		 */
 		

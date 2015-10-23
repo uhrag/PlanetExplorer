4536a5b1a0e3434881a3992ba0f6f0e452baccd3
somename <someemail>
2015-10-23 12:08:57 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/Planet.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/Planet.java
@@ -1,5 +1,26 @@
 package org.unioulu.tol.sqat2015.planetExplorer;
 
-public class Planet {
+public class Planet  {
+
+	private int width;
+	private int height;
+	private boolean[][] size;
+	
+	public Planet( int x, int y) {
+		width = x;
+		height = y;
+		size = new boolean[x][y];
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	
+	public boolean size(int x, int y) {
+		return size[x][y];
+	}
 
 }

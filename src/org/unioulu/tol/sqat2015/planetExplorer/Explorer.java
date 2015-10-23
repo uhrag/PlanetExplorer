bcbb86cc1db002c594c300756bb91448d4cc4dfd
somename <someemail>
2015-10-23 12:17:36 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat2015/planetExplorer/Explorer.java
+++ src/org/unioulu/tol/sqat2015/planetExplorer/Explorer.java
@@ -1,5 +1,29 @@
 package org.unioulu.tol.sqat2015.planetExplorer;
 
 public class Explorer {
-
+	private int posX;
+	private int posY;
+	private String direction;
+	
+	public void Explorer(int x, int y, String direction) {
+		
+	}
+	
+	public void Landing(){
+		posX = 0;
+		posY = 0;
+		direction = "North";
+	}
+	
+	public int getPosX(){
+		return posX;
+	}
+	
+	public int getPosY(){
+		return posY;
+	}
+	
+	public String getDirection(){
+		
+	}
 }
